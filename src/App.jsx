eeb79945f38aea2c3c030ecc153b014cbc02a194
BlesUstetu <lionustetu@gmail.com>
2026-03-09 15:57:29 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import WalletConnect from "./components/WalletConnect"
+import Vault from "./components/Vault"
+
+export default function App(){
+
+return(
+<div style={{padding:40}}>
+<h1>BaseParkVault</h1>
+<WalletConnect/>
+<Vault/>
+</div>
+)
+
+}
