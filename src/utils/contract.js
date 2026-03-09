e19be00c32f265f61f7b79d05dd47c032091e364
BlesUstetu <lionustetu@gmail.com>
2026-03-09 16:02:32 +0700

    Create contract.js

---
+++ src/utils/contract.js
@@ -0,0 +1,11 @@
+
+export const CONTRACT_ADDRESS = "PASTE_CONTRACT_ADDRESS"
+
+export const ABI = [
+"function deposit() payable",
+"function withdraw(uint256 amount)",
+"function emergencyWithdraw()",
+"function claimReward()",
+"function pendingReward(address) view returns(uint256)",
+"function totalAssets() view returns(uint256)"
+]
