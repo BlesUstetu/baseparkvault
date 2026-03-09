dd1e9697d6000a9f8aa545cc89831c3f4f07f80c
BlesUstetu <lionustetu@gmail.com>
2026-03-09 13:37:18 +0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import WalletConnect from "./components/WalletConnect"
+import Vault from "./components/Vault"
+
+export default function App(){
+
+return(
+<div style={{padding:40}}>
+<h1>BaseParkVault</h1>
+<WalletConnect/>
+<Vault/>
+</div>
+)
+
+}
