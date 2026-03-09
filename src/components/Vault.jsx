25e26b16e53c308dee5dfece755f32d55436df11
BlesUstetu <lionustetu@gmail.com>
2026-03-09 16:25:12 +0700

    Update Vault.jsx

--- src/components/Vault.jsx
+++ src/components/Vault.jsx
@@ -1,4 +1,3 @@
-
 import {useState} from "react"
 import {ethers} from "ethers"
 import {CONTRACT_ADDRESS,ABI} from "../utils/contract"
@@ -18,11 +17,11 @@ const tx = await contract.deposit({
 value: ethers.parseEther(amount)
 })
 
-setStatus("deposit pending")
+setStatus("Deposit pending...")
 
 await tx.wait()
 
-setStatus("deposit success")
+setStatus("Deposit success")
 
 }
 
@@ -36,11 +35,11 @@ const tx = await contract.withdraw(
 ethers.parseEther(amount)
 )
 
-setStatus("withdraw pending")
+setStatus("Withdraw pending...")
 
 await tx.wait()
 
-setStatus("withdraw success")
+setStatus("Withdraw success")
 
 }
 
@@ -54,29 +53,40 @@ const tx = await contract.emergencyWithdraw()
 
 await tx.wait()
 
-setStatus("emergency withdraw executed")
+setStatus("Emergency withdraw executed")
 
 }
 
 return(
 
-<div style={{marginTop:20}}>
+<div>
 
 <input
+className="input"
 placeholder="ETH amount"
 value={amount}
 onChange={(e)=>setAmount(e.target.value)}
 />
 
-<div style={{marginTop:10}}>
+<div>
 
-<button onClick={deposit}>Deposit</button>
-<button onClick={withdraw}>Withdraw</button>
-<button onClick={emergency}>Emergency Withdraw</button>
+<button className="btn" onClick={deposit}>
+Deposit
+</button>
+
+<button className="btn" onClick={withdraw}>
+Withdraw
+</button>
+
+<button className="btn btnDanger" onClick={emergency}>
+Emergency Withdraw
+</button>
 
 </div>
 
-<p>{status}</p>
+<div className="status">
+{status}
+</div>
 
 </div>
 
