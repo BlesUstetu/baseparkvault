501e4be67da47e2de3943905c10a3fea2cc31f15
BlesUstetu <lionustetu@gmail.com>
2026-03-09 16:33:42 +0700

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+export default function Navbar(){
+
+return(
+
+<div className="navbar">
+
+<div className="logo">
+BaseParkVault
+</div>
+
+<div className="navLinks">
+Vault
+Stats
+Docs
+</div>
+
+</div>
+
+)
+
+}
