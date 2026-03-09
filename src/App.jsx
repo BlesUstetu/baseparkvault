6fc3bbbdea46aa7558ee2929db4dac08bc51e061
BlesUstetu <lionustetu@gmail.com>
2026-03-09 16:20:58 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-
 import WalletConnect from "./components/WalletConnect"
 import Vault from "./components/Vault"
 
 export default function App(){
 
 return(
-<div style={{padding:40}}>
-<h1>BaseParkVault</h1>
+
+<div className="container">
+
+<div className="title">BaseParkVault</div>
+
 <WalletConnect/>
+
 <Vault/>
+
+<div className="tvl">
+Vault TVL : loading...
 </div>
+
+</div>
+
 )
 
 }
